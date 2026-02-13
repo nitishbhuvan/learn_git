50634924b8e4d0ef52c0dfab6a591b271204762c
nitishbhuvan <bhuvanitish@gmail.com>
2026-02-13 15:36:55 +0530

    Code written for inputing and searching an element

--- hello.py
+++ hello.py
@@ -1,1 +1,12 @@
-print("Hello world")
+lis=[]
+n=int(input("Enter the number of elements: "))
+for i in range (n):
+    lis.append(int(input("Enter the list: ")))
+    
+print(lis)
+id=1
+a=int(input("Enter the element and get its position: "))
+for i in lis:
+    if(i==a):
+        print(f"{a} found at {id}")
+    id+=1
