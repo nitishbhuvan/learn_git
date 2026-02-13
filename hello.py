97752dbf6e4e7369a3b05655290bb50ea931176c
nitishbhuvan <bhuvanitish@gmail.com>
2026-02-13 23:12:59 +0530

    Changed the search algorithm to binary search

--- hello.py
+++ hello.py
@@ -4,10 +4,18 @@ for i in range (n):
     lis.append(int(input("Enter the list: ")))
     
 print(lis)
-id=1
+lis.sort()
+print("Sorted list: ",lis)
 a=int(input("Enter the element and get its position: "))
-for i in lis:
-    if(i==a):
-        print(f"{a} found at {id}")
-    id+=1
+high=n-1
+low=0
+while (low<=high):
+    mid=(low+high)//2
+    if (lis[mid]==a):
+        print(f"{a} found at {mid+1}")
+        break
+    elif (lis[mid]>a):
+        high=mid-1
+    else:
+        low=mid+1
 print("Indexing starts from 1")
